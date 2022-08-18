4106dc843d13a3f15fb36edee7f509b7c4f90be0
DESKTOP-T0HDS8P\dell <somnathveerappan@gmail.com>
2022-08-18 19:43:11 +0530

    updated one python program

--- batch_2/33_sum three value.py
+++ batch_2/33_sum three value.py
@@ -1,6 +1,13 @@
 # Write a Python program to sum of three given integers.
 # However, if two values are equal sum will be zero.
+
+#Solution:
+
+test=1
+
 def fun(num_1,num_2,num_3):
+    global test
+    print("\nTest-->",test)
     try:
         num_1 = int(num_1)
         num_2 = int(num_2)
@@ -12,13 +19,15 @@ def fun(num_1,num_2,num_3):
         print("sum three numbers is :", sum)
     except:
         print("Invalid Input")
-       
+    test+=1
+
+#Tests
 fun(3,4,5)
 fun(5,7,5)
 fun(-5,7,-5)
 fun(-5,7,5)
 fun(-5,-6,-7)
-fun('s','h','i')
+fun('s','h','ig')
 fun(-3,6,-5)
 fun('3','4','5')
 
