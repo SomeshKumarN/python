a2e2d791f12e00205efb68b86b4baba2630a08a2
DESKTOP-T0HDS8P\dell <somnathveerappan@gmail.com>
2022-08-21 21:34:14 +0530

    updated this programs

--- batch_2/39_calculate interest amount.py
+++ batch_2/39_calculate interest amount.py
@@ -3,7 +3,18 @@
 # Expected Output : 12722.79
 def future_amount(amount,interest,years):
     
-    return amount*((1+(0.01*interest))**years)
-print(future_amount(10000 ,3.5 ,7))
+    return amount*((1+(0.01*interest))**years) 
 
 
+def futureAmt(p,r,t):
+# p = int(input("Enter the principle amount:"))
+# r = float(input("Enter the rate of interest:"))
+# t = int(input("Enter the tenure in years:"))
+    a= p*(1+(r*t))
+    print("Future value of your loan would be-->",a)
+
+
+print(future_amount(100000 ,12,1))
+print(future_amount(5000,7,10))
+futureAmt(100000,0.12,1)
+futureAmt(5000,0.07,10)
--- batch_2/42_check python shell bit.py
+++ batch_2/42_check python shell bit.py
@@ -1,3 +1,7 @@
 # Write a Python program to determine if a Python shell is executing in 32bit or 64bit mode on OS.
+# from platform import architecture, platform 
+from platform import architecture, platform
 import struct
 print(struct.calcsize('P')*8)
+
+print(architecture())
