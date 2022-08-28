5b89c2dc90044f84dd03d9a9abe5dac9cce8890a
SomnathVeerappan <110618993+SomnathVeerappan@users.noreply.github.com>
2022-08-28 22:15:33 +0530

    Delete 53_access_environment_variables.py

--- 53_access_environment_variables.py
+++
@@ -1,7 +0,0 @@
-# Write a python program to access environment variables.
-
-import os
-print(os.environ)
-print(os.environ['onedriveconsumer'])
-print(os.environ['computername'])
-print(os.environ['username'])
