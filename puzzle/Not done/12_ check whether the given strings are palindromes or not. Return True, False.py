dd84b7a12ed61bbfde71761d347e55eb2f32b566
DESKTOP-T0HDS8P\dell <somnathveerappan@gmail.com>
2022-09-01 20:39:20 +0530

    deleted

--- puzzle/Not done/12_ check whether the given strings are palindromes or not. Return True, False.py
+++
@@ -1,8 +0,0 @@
-"""
-Write a Python program to check whether the given strings are palindromes or not. Return True, False.
-Input:
-['palindrome', 'madamimadam', '', 'foo', 'eyes']
-Output:
-[False, True, True, False, False]
-
-"""
