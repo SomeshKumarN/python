9953778c3ac220f66d31ad01c6a2b885e4ced981
SomeshkumarN <somesh022@hotmail.com>
2025-05-26 21:37:18 -0400

    dataTypes

---
+++ dataTypes.py
@@ -0,0 +1,64 @@
+# a =10
+
+# def add(var1, var2):
+#     global a
+#     a = 20 
+#     b=10   
+
+#     print("Adding two variables")
+#     print(f"Variable 1: {var1}")
+#     print(f"Variable 2: {var2}")
+#     print(f"Result: {var1 + var2}")
+    
+
+# def subtract(var1, var2):
+#     d=10
+#     def method1(var1, var2):
+#         c=10
+#         return var1 + var2
+#     return var1 - var2
+
+# print("This is a test file for variables.py")
+# add(5, 10)
+
+
+# print(f"Variable a: {a}")
+# # print(b)
+
+
+# "somesh kumar" --- kumar
+# "selva bharathi" --- bharathi
+# "praveen kumar" --- kumar
+# "venkataswaran sdfsf" --- sdfsf
+# "abc xzy" --- xzy
+
+
+# name ="Somesh Kumars"
+# name ="Selva Bharathi-SNew"
+# name ="Thirumal is Selvan-YSNew"
+# name ="Sindu Devi-SDed"
+# # print(name.index("R"))
+# # print(name.index("i"))
+# # print(name.index("c"))
+# # print(name.index("h"))
+# startPosition = name.index(" ")
+# endPositino = name.index("-")
+# result = name[startPosition+1 :endPositino:1]
+
+# print(name[startPosition+1 :endPositino:1])
+# print(name[startPosition+1 :endPositino:2])
+# print(name[startPosition+1 :endPositino:3])
+# print(name[startPosition+1 :endPositino:4])
+
+
+def isValidEmail(email):
+    return True
+name = "Somesh Kumar"
+# isValid = 
+
+if(name.__contains__('Som1')):
+    {
+    print("Valid")
+    }
+else:
+    print("Invalid")  
