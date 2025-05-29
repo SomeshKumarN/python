f157ba9b601354b6de5c7ba28f853e14f890eb31
SomeshkumarN <somesh022@hotmail.com>
2025-05-28 20:15:42 -0400

    DataType Concepts added

---
+++ 1_dataTypes.py
@@ -0,0 +1,46 @@
+# Following are the basic Data Types in Python:
+# 1. String - "Hello World"
+# 2. Integer - 10
+# 3. Float - 10.5
+# 4. Boolean - True/False
+# 5. List - [1, 2, 3, 4, 5]
+# 6. Tuple - (1, 2, 3, 4, 5)
+# 7. Dictionary - {"key1": "value1",
+#                  "key2": "value2"}
+# 8. Set - {1, 2, 3, 4, 5}
+
+# Example usage of each data type:
+
+# String
+my_string = "Hello World"
+print("String:", my_string)
+
+# Integer
+my_int = 10
+print("Integer:", my_int)
+
+# Float
+my_float = 10.5
+print("Float:", my_float)
+
+# Boolean
+my_bool = True
+print("Boolean:", my_bool)
+
+# List
+my_list = [1, 2, 3, 4, 5]
+print("List:", my_list)
+
+# Tuple
+my_tuple = (1, 2, 3, 4, 5)
+print("Tuple:", my_tuple)
+
+# Dictionary
+my_dict = {"key1": "value1", "key2": "value2"}
+print("Dictionary:", my_dict)
+
+# Set
+my_set = {1, 2, 3, 4, 5}
+print("Set:", my_set)
+
+
